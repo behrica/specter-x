0fe22509de2ab708769b8ab02e53a606e6142ded
Carsten Behring <carsten.behring@gmail.com>
2018-03-20 08:39:34 +0100

    added deploy target

--- project.clj
+++ project.clj
@@ -1,6 +1,7 @@
-(defproject behrica/specter-x "0.1.0"
+(defproject behrica/specter-x "0.1.1-SNAPSHOT"
   :description "Extensions to specter to explore data structures"
   :url "http://example.com/FIXME"
+  :lein-release {:deploy-via :clojars}
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.9.0"]
